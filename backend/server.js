df709301c8bebe41c8fb4a37fba72381afbd9e57
Rakshit Harsh <rakshitharsh7@gmail.com>
2024-07-11 23:01:02 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+
+import connectToMongoDB from './db/connectToMongodb.js';
+import cookieParser from 'cookie-parser'
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+
+    res.send("Hello, world!");
+})
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`)
+})
