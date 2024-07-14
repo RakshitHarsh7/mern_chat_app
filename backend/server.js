ca6c4cb7f424c769fa20265b351519d9d851082b
Rakshit Harsh <rakshitharsh7@gmail.com>
2024-07-14 16:52:50 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,6 +11,8 @@ import userRoutes from "./routes/user.routes.js"
 import connectToMongoDB from './db/connectToMongodb.js';
 import cookieParser from 'cookie-parser'
 import { server, app } from './socket/socket.js';
+
+const __dirname = path.resolve();
 
 // const app = express();
 
@@ -26,6 +29,12 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
